cbd267db42468ede13c92d6b833c6f55adf2c17b
Vinicius Gouveia <gouveiavinicius96@gmail.com>
2023-04-22 14:37:22 -0300

    remov

--- index.js
+++ index.js
@@ -2,20 +2,6 @@ const express = require('express');
 const server = express();
 const elementos = require('./src/data/elementos.json')
 
-// server.listen(4000, () => {
-//   console.log("Servidor ativo");
-// });
-
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
-
-server.configure(function() {
-  server.use(allowCrossDomain);
-})
 
 server.listen(process.env.PORT || 3000);
 
