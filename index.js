160f612d99e171f6620e5ac703242d9ae95ecdf8
Vinicius Gouveia <gouveiavinicius96@gmail.com>
2023-04-23 01:40:06 -0300

    server

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
+app.listen(3333, () => console.log('server is running on port 3333'));
+
 const elements = require('./src/data/elementos.json')
 
 app.get('/', (req, res) => res.status(200).send('Hello Word'));
 
-app.get('/elements', (req, res) => res.status(200).send(elements));
+app.get('/elements', (req, res) => res.send(elements));
 
-app.listen(3333, () => console.log('server is running on port 3333'));
 
 
